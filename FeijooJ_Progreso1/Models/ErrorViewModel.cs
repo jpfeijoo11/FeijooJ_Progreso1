623f1b3105cbc608bc1d049b887d20321ac561c3
Juan <juanpablote10@hotmail.com>
2025-04-29 07:18:33 -0500

    Agregar archivos de proyecto.

---
+++ FeijooJ_Progreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeijooJ_Progreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
